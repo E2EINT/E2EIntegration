3e3bbfa60b534b5e0a6c94919f9fe5267f623f04
ganesan <ganesan@10.93.232.216>
2017-09-28 12:09:28 +0530

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/tc_002.java
@@ -0,0 +1,23 @@
+package testcases;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class tc_002{
+	
+		
+@Test
+public void testcase()
+{
+	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/Driver/chromedriver");
+	ChromeDriver driver  = new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.findElementById("email").sendKeys("ganesan");
+	driver.findElementById("pass").sendKeys("ganesh");
+	driver.quit();
+	
+			
+}
+
+
+
+}
